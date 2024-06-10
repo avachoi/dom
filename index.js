d060c4d451146354485298e225e64500c2419a61
avachoi <alexbchoi@gmail.com>
2024-06-10 13:23:01 -0400

    created a form

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-const app = document.getElementById("app");
-const main = document.createElement("div");
-app.appendChild(main);
-main.id = "main";
-const input = document.createElement("input");
-main.appendChild(input);
-
 const header = document.querySelector("h1");
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const todoInput = form.elements["todoInput"];
+const lists = document.getElementById("lists");
+
+function addTodo(e) {
+	e.preventDefault();
+	const newTask = todoInput.value;
+	let todo = document.createElement("li");
+	todo.innerHTML = newTask;
+	lists.appendChild(todo);
+	todoInput.value = "";
+}
+
+form.addEventListener("submit", (e) => addTodo(e));
+
 header.style.fontFamily = "Gill Sans";
+
+// const app = document.getElementById("app");
+// const main = document.createElement("div");
+// app.appendChild(main);
+// main.id = "main";
+// const input = document.createElement("input");
+// main.appendChild(input);
+
+// input.addEventListener()
