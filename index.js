a3082a40d0a0f6668d785e336f8f61b585f7e66b
avachoi <alexbchoi@gmail.com>
2024-06-10 12:00:51 -0400

    started

---
+++ index.js
@@ -0,0 +1,9 @@
+const app = document.getElementById("app");
+const main = document.createElement("div");
+app.appendChild(main);
+main.id = "main";
+const input = document.createElement("input");
+main.appendChild(input);
+
+const header = document.querySelector("h1");
+header.style.fontFamily = "Gill Sans";
