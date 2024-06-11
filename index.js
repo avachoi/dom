df7c795e9573bf08cb96cd0f7283123e33576c20
avachoi <alexbchoi@gmail.com>
2024-06-10 23:39:41 -0400

    create todoList with delete

--- index.js
+++ index.js
@@ -3,30 +3,38 @@ const main = document.getElementById("main");
 const form = document.getElementById("form");
 const todoInput = form.elements["todoInput"];
 const lists = document.getElementById("lists");
+const addTodoButton = document.getElementById("addTodoButton");
 
 lists.style.backgroundColor = "rgb(247, 238, 240)";
 todoInput.style.width = "350px";
 todoInput.style.margin = "20px";
 header.style.textAlign = "center";
 
-function addTodo(e) {
-	e.preventDefault();
-	const newTask = todoInput.value;
-	let todo = document.createElement("li");
-	todo.innerHTML = newTask;
-	lists.appendChild(todo);
-	todoInput.value = "";
+function deleteTodo(e) {
+	const todoItem = e.target.parentElement;
+	todoItem.remove();
+	alert("will be deleted");
 }
 
-form.addEventListener("submit", (e) => addTodo(e));
+function addTodo(e) {
+	const newTask = todoInput.value.trim();
+	if (newTask.length > 0) {
+		let todo = document.createElement("li");
+		todo.innerHTML = newTask;
+		todo.style.fontSize = "20px";
+		const deleteButton = document.createElement("button");
+		deleteButton.innerHTML = "delete";
+		todo.appendChild(deleteButton);
+		lists.appendChild(todo);
+		deleteButton.addEventListener("click", deleteTodo);
+		todoInput.value = "";
+		window.scrollTop = lists.scrollHeight;
+	} else {
+		alert("Todo cannot be empty");
+		todoInput.focus();
+	}
+}
+
+addTodoButton.addEventListener("click", addTodo);
 
 header.style.fontFamily = "Gill Sans";
-
-// const app = document.getElementById("app");
-// const main = document.createElement("div");
-// app.appendChild(main);
-// main.id = "main";
-// const input = document.createElement("input");
-// main.appendChild(input);
-
-// input.addEventListener()
